86012825dcec1a3c001ac2589a206e4f44ce3907
Jonsteph93 <jstephanmv@gmail.com>
2021-10-11 23:00:14 -0500

    login page update, some JS added

--- Functions.js
+++ Functions.js
@@ -21,4 +21,39 @@ class Template extends HTMLElement{
 
 customElements.define('coll-navbar', Template);
 
+document.addEventListener("DOMContentLoaded", () =>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
 
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form__login");
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form__create")
+    });
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        createAccountForm.classList.remove("form__create");
+        createAccountForm.classList.add("form--hidden");
+        loginForm.classList.remove("form--hidden");
+        loginForm.classList.add("form__login")
+    });
+});
+
+{
+    const collapsedClass = "nav--collapsed";
+    const lsKey = "navCollapsed";
+
+    const nav = document.querySelector(".nav");
+    const navBorder = nav.querySelector(".nav__border");
+
+    if(localStorage.getItem(lsKey) === "true"){
+        nav.classList.add(collapsedClass);
+    }
+
+    navBorder.addEventListener("click",() =>{
+        nav.classList.toggle(collapsedClass);
+        localStorage.setItem(lsKey, nav.classList.contains(collapsedClass));
+    });
+}

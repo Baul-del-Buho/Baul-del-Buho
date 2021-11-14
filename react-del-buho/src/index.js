14ef40b905c3b1f61ca1c8702b776965df3e5ebd
Jonsteph93 <jstephanmv@gmail.com>
2021-11-13 20:29:40 -0500

    Test files

--- react-del-buho/src/index.js
+++ react-del-buho/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -32,24 +32,190 @@ const element =
 <p className='main-text' >lorem ipsum dolor sit amet</p>
 </div>;*/
 
-const tick = () => {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
+// const TheClock = (props) =>{
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+
+// class TheClock extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {date: new Date()}
+//   }
   
-  ReactDOM.render(
-    <React.StrictMode>
-    {element}
-  </React.StrictMode>,
-  document.getElementById('root')
-  );
+//   componentDidMount(){
+//     this.timerID = setInterval(
+//      () => this.tick(),
+//      1000
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
+
+//   render (){
+//     return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//     </div>
+//     );
+//   }
+// }
+
+//   ReactDOM.render(
+//     // <React.StrictMode>
+//       <TheClock />,
+//   //{ </React.StrictMode>, }
+//   document.getElementById('root')
+//   );
+  // setInterval(tick, 1000);
+
+
+// function TheWelcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// class ClassWelcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>;
+//   }
+// }
+// const nombre ="Jonathan";
+
+// const MainApp = (props) => {
+//   return (
+//   <div className="welcome-container"> 
+//   <TheWelcome name={ nombre } />
+//   <TheWelcome name="Stephan" />
+//   <TheWelcome name="Grupo 01" />
+//   </div>
+//     );
+// }
+
+// const usuarios = {
+//   jonsteph : {
+//     name : "Jonsteph  Meza",
+//     description : "sdjjf jdh fjd  d hdfjdkjhjsk  djhdjd  s dkjshdj s  jdhj d jdhdjs j sjdhjs d hjkjfkhsaska jjhsduk" ,
+//     image: "https://placeimg.com/300/300/people"
+//   },
+//   grupo : {
+//     name : "Grupo 01",
+//     description : "sdjjf jdh fjd  d hdfjdkjhjsk  djhdjd  s dkjshdj s  jdhj d jdhdjs j sjdhjs d hjkjfkhsaska jjhsduk" ,
+//     image: "https://placeimg.com/300/300/tech"
+//   },
+//   nuevo : {
+//     name : "Un nuevo elemento",
+//     description : "sdjjf jdh fjd  d hdfjdkjhjsk  djhdjd  s dkjshdj s  jdhj d jdhdjs j sjdhjs d hjkjfkhsaska jjhsduk" ,
+//     image: "https://placeimg.com/300/300/animals"
+//   }
+// }
+
+// const UserAvatar = (props) => {
+//   return(
+//     <div className="user-avatar">
+//         <img className="user-avatar__image" src={props.user.image} alt={props.user.name} />
+//       </div>
+//   );
+// }
+
+// const UserInfo = (props) => {
+//   return (
+//     <div className="user-info">
+//         <h2 className="user-info__name">{props.user.name}</h2>
+//         <hr/>
+//         <p className="user-info__description">{props.user.description}</p>
+//       </div>
+//   );
+// }
+
+// const UserCard = (props) => {
+//   return (
+//     <div className="user-card">
+//       <UserAvatar user={props.user}/>
+//       <UserInfo user={props.user}/>
+//     </div>
+//   );
+// }
+
+
+// ReactDOM.render(
+//     <React.StrictMode>
+
+//   </React.StrictMode>,
+//   document.getElementById('root')
+//   );
+
+
+  
+  {/* <div className="card-container">  
+    <UserCard user={usuarios.jonsteph}/>
+    <UserCard user={usuarios.grupo}/>
+    <UserCard user={usuarios.nuevo}/>
+  </div> */}
+
+//   function Form() {
+//     function handleSubmit(e) {
+//       e.preventDefault();    console.log('You clicked submit.');
+//     }
+  
+//     return (
+//       <form onSubmit={handleSubmit}>
+//         <button type="submit">Submit</button>
+//       </form>
+//     );
+//   }
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//       <Form />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+//   );
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback    
+    this.handleClick = this.handleClick.bind(this);
+    }
+
+  handleClick() {    this.setState(prevState => ({ isToggleOn: !prevState.isToggleOn}));  }
+  render() {
+    return (
+      <div id="button-enclosure">
+      <div>
+        
+      
+      <button onClick={this.handleClick}> {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+      <p>{this.state.isToggleOn ? 'Lights On!' : 'Nothing'}</p>
+      </div>
+      </div>
+    );
+  }
 }
-  setInterval(tick, 1000);
-  
-// If you want to start measuring performance in your app, pass a function
+
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
+
+  // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

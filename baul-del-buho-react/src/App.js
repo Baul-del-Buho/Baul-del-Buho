7e3f3210dbf426d9f37edfa079bb8ca0e467e223
Juan Diego Garcia <juandiego0827@gmail.com>
2021-11-13 20:27:06 -0500

    Issue 1.1-s1

--- baul-del-buho-react/src/App.js
+++ baul-del-buho-react/src/App.js
@@ -3,11 +3,13 @@ import {
     BrowserRouter as Router
 } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Carrusel from './components/Carrusel';
 
 function App () {
     return (
         <Router>
             <Navbar />
+            <Carrusel />
         </Router>
     )
 }

a89c5abc6c205af31c5f3edea0e41dc30ddc2f9b
Jonsteph93 <jstephanmv@gmail.com>
2021-12-06 18:39:23 -0500

    undo

---
+++ OLD/baul-del-buho/src/components/CollapsibleNav.vue
@@ -0,0 +1,94 @@
+<template>
+   <div class="page-container">
+        <nav class="nav">
+            
+            
+        
+         <!--Menú desplegable para la navegación-->
+         
+
+                    <router-link class="nav__link" :to="{name:'Home'}"><div class="nav__icon-container">
+                     <i class="material-icons">dashboard</i>
+                 </div>
+                 <span class="nav__label">Inicio</span></router-link>
+                    <router-link class="nav__link" :to="{name: 'Proyectos'}"><div class="nav__icon-container">
+                     <i class="material-icons">construction</i>
+                 </div>
+                 <span class="nav__label">Proyectos</span></router-link>
+                    <router-link class="nav__link" :to="{name: 'InicioSesion'}"><div class="nav__icon-container">
+                     <i class="material-icons">account_circle</i>
+                 </div>
+                 <span class="nav__label">Inicio de Sesión</span></router-link>
+                    <router-link class="nav__link" :to="{name: 'About'}"><div class="nav__icon-container">
+                     <i class="material-icons">groups</i>
+                 </div>
+                 <span class="nav__label">¿Quiénes somos?</span></router-link>         
+             
+             
+             
+        </nav>
+          </div>       
+</template>
+<script>
+export default {
+    name: "CollapsibleNav.vue",
+};
+
+</script>
+
+
+
+<style scoped>
+.nav{
+    display:inline-block;
+    width: 200px;
+    height:inherit;
+    position: relative;
+    background-color: rgb(240,240,250);
+    transition:width 0.2s;
+    flex-shrink: 0;
+}
+.nav__link{
+    display:flex;
+    align-items:center;
+    color:rgba(0,0,100,0.75);
+    text-decoration: none;
+}
+.nav__link:hover{
+    background-color: lightgray;
+}
+
+.nav__icon-container{
+    width:var(--nav-icon-width);
+    height:var(--nav-icon-width);
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+}
+
+.nav__label{
+    white-space: nowrap;
+}
+
+.nav__border{
+    position:absolute;
+    left:100%;
+    top:0;
+    width: var(--nav-border-width);
+    height:100%;
+    background-color: rgb(150,150,150);
+    transition: background-color 0.2s;
+    cursor:ew-resize;
+}
+.nav__border:hover{
+    background-color: rgb(100,100,100);
+}
+.nav--collapsed{
+    width: var(--nav-icon-width);
+
+}
+.nav--collapsed .nav__label{
+    display:none;
+}
+</style>
---
+++ OLD/baul-del-buho/src/views/InicioSesion.vue
@@ -0,0 +1,74 @@
+<template>
+    <main>
+    <div class="login-container"></div>
+    <form class="form__login" id="login">
+        <h2 class="form__title">Iniciar Sesión</h2>
+        <div class="form__message form__message--error"></div>
+        <div class="form__input-group">
+            <input type="text" class="form__input" autofocus placeholder="Email o nombre de usuario">
+            <div class="form__input-error-message"></div>
+        </div>
+        <div class="form__input-group">
+            <input type="text" class="form__input" autofocus placeholder="Contraseña">
+            <div class="form__input-error-message"></div>
+        </div>
+        <button class="form__button" type="submit">Ingresar</button>
+        <p class="form__text">
+            <a href="#" class="form__link">¿Olvido su contraseña?</a>
+        </p>
+        <p class="form__text">
+            <a href="./" id="linkCreateAccount" class="form__link">¿No posee una cuenta? Crear una</a>
+        </p>
+    </form>
+
+    <form class="form--hidden" id="createAccount">
+        <h2 class="form__title">Crear Cuenta</h2>
+        <div class="form__message form__message--error"></div>
+        <div class="form__input-group">
+            <input type="text" class="form__input" autofocus placeholder="Nombre de usuario">
+            <div class="form__input-error-message"></div>
+        </div>
+        <div class="form__input-group">
+            <input type="text" class="form__input" autofocus placeholder="Dirección de correo">
+            <div class="form__input-error-message"></div>
+        </div>
+        <div class="form__input-group">
+            <input type="text" class="form__input" autofocus placeholder="Contraseña">
+            <div class="form__input-error-message"></div>
+        </div>
+        <div class="form__input-group">
+            <input type="text" class="form__input" autofocus placeholder="Confirmar contraseña">
+            <div class="form__input-error-message"></div>
+        </div>
+        <button class="form__button" type="submit">Continuar</button>
+        
+        <p class="form__text">
+            <a href="./" id="linkLogin" class="form__link">¿Ya está registrado? Iniciar sesión</a>
+        </p>
+    </form>
+</main>
+</template>
+<script>
+document.addEventListener("DOMContentLoaded", () =>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form__login");
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form__create")
+    });
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        createAccountForm.classList.remove("form__create");
+        createAccountForm.classList.add("form--hidden");
+        loginForm.classList.remove("form--hidden");
+        loginForm.classList.add("form__login")
+    });
+});
+</script>
+<style scoped>
+
+</style>
